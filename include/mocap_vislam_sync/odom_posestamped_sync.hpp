72b200c7708a15d53d272601b1806021c3610578
marcelino-pensa <marcelino@pensasystems.com>
2021-01-07 11:16:46 -0600

    allow sync of odometry with posestamped

---
+++ include/mocap_vislam_sync/odom_posestamped_sync.hpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2020 by Pensa Systems, Inc. -- All rights reserved
+// Confidential and Proprietary
+
+#pragma once
+
+#include "ros/ros.h"
+
+// ROS types
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <std_srvs/Trigger.h>
+
+// Eigen
+#include <Eigen/Core>
+
+// Locally-defined libraries
+#include "mocap_vislam_sync/msg_conversions.hpp"
+
+namespace sync_topics {
+
+typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseStamped, nav_msgs::Odometry> SyncPolicyApprox;
+typedef message_filters::Synchronizer<SyncPolicyApprox> Sync;
+
+class OdomPoseStampedSync {
+ public:
+    OdomPoseStampedSync(ros::NodeHandle *nh);
+
+ private:
+    ros::NodeHandle nh_;
+    std::string in_pose_stamped_topic_, in_odometry_topic_;
+    std::string output_file_path_;
+    boost::shared_ptr<Sync> sync_;
+    message_filters::Subscriber<geometry_msgs::PoseStamped>* pose_stamped_sub_;
+    message_filters::Subscriber<nav_msgs::Odometry>* odometry_sub_;
+    std::vector<std::pair<Eigen::Vector3d, Eigen::Vector3d> > position_pair_vec_;
+    std::vector<std::pair<double, double> > yaw_pair_vec_;
+    std::vector<double> times_;
+    ros::Time initial_time_;
+    bool start_new_estimation_, is_estimating_;
+    ros::ServiceServer start_scale_estimation_srv_, stop_scale_estimation_srv_;
+    std::string node_name_;
+
+    // Callback that subscribes to both mocap and vislam measurements
+    void PoseCallback(const geometry_msgs::PoseStamped::ConstPtr& mocap_msg,
+                      const nav_msgs::Odometry::ConstPtr& vislam_msg);
+
+    // Service to trigger the computation of scale
+    bool StartSyncSrv(std_srvs::Trigger::Request  &req,
+                      std_srvs::Trigger::Response &res);
+    bool StopSyncSrv(std_srvs::Trigger::Request  &req,
+                     std_srvs::Trigger::Response &res);
+};
+
+}  // namespace sync_topics
---
+++ src/odom_pose_sync.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 by Pensa Systems, Inc. -- All rights reserved
+// Confidential and Proprietary
+
+#include "ros/ros.h"
+#include "mocap_vislam_sync/odom_posestamped_sync.hpp"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "odom_pose_stamped_sync");
+  ros::NodeHandle node("~");
+
+  sync_topics::OdomPoseStampedSync sync1(&node);
+
+  ros::spin();
+
+  return 0;
+}
